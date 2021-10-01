8b7822faabfaaea46dceca67f8b2ce28c627e3fc
Milton Bedah <miltonbedah@gmail.com>
2021-10-01 09:09:33 +0300

    Add existing file

--- public/index.js
+++ public/index.js
@@ -4,3 +4,29 @@ const menu = document.querySelector('#menu');
 icon.addEventListener('click', ()=>{
    menu.classList.toggle('hidden')
 });
+
+
+// Set the date we're counting down to
+let countDownDate = new Date("Oct 20, 2021 00:00:00").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+  // Get today's date and time
+  let now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hrs").innerHTML = hours;
+  document.getElementById("mins").innerHTML = minutes;
+  document.getElementById("secs").innerHTML = seconds;
+})
